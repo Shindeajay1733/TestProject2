80120cc92ed874127ed77144d2aa31d6a8fad9aa
Administrator <Administrator@DESKTOP-FVVKFHI>
2022-02-22 22:31:36 +0530

    second project commit

---
+++ AutomationTesting/src/selelniumAutomation/TestClass.java
@@ -0,0 +1,14 @@
+package selelniumAutomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClass 
+{
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\Downloads\\chromedriver_win32\\chromedrivere.exe");
+		WebDriver driver = new ChromeDriver();
+		
+	}
+}
